2f3518b6e64694c59b9595b51540cd7ff34c46f8
Mohammad Aljermy <121159582+Mkaljermy@users.noreply.github.com>
2024-02-28 19:45:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ prompt = PromptTemplate(
 
 def get_response(question):
     model = ChatGoogleGenerativeAI(model = 'gemini-pro', google_api_key=os.getenv("GOOGLE_API_KEY"),
-                              temperature=0.7)
+                              temperature=0)
     
     pdf_loader = PyPDFLoader('Palestine.pdf')
 
